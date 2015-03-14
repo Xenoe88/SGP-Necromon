bb0cee40a75f6f630a4f6b110d96beb7ff4c924d
Eric Truitt <erictruitt@fullsail.edu>
2015-03-14 15:23:31 -0400

    pre-entitiy pull commit

--- Straw_Man/Assets/Scriptes/Player Controller.cs
+++ Straw_Man/Assets/Scriptes/Player Controller.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
+    //start 2:30 pm
+    //end 
+
+    public float movement;
+    public int jumpHeight;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float groundRadius;
+    public bool isGrounded;
+    public int jumpCount;
+    public int fallRate;
+    public GameObject jumpParticles;
+    public int numActiveNecromon;
+    public GameObject hitBox;
+    public Entity player;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
 }
