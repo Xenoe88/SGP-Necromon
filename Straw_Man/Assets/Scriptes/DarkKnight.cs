5219b944381a6be10e3abb4c3ffcb4dcb12e72c8
Eric Truitt <erictruitt@fullsail.edu>
2015-03-27 13:36:14 -0400

    eric truitt

---
+++ Straw_Man/Assets/Scriptes/DarkKnight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarkKnight : MonoBehaviour
+{
+    private Animator m_animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ResetAnim() { m_animator.SetInteger("KnightAnim", 0); }
+    void Destroy() { Destroy(gameObject); }
+}
