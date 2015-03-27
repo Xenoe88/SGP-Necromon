6f6c354cf79e0f1dd7e6ebedbac760d995323945
Zeeshan Majeed <majeedzee@hotmail.com>
2015-03-26 20:26:50 -0400

    Added turret enemy and a projectile for him. - Zeeshan Majeed

---
+++ Straw_Man/Assets/Turret.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+    public Entity m_Entity;
+
+    public bool m_moving = true;
+    public bool m_isNecro = false;
+
+    public GameObject m_target;
+    public Projectile m_proj;
+	// Use this for initialization
+	void Start () {
+        m_Entity = GetComponent<Entity>();
+        m_Entity.m_dmg = 10;
+        m_Entity.m_health = 20;
+        m_Entity.m_attackCooldown = 2.0f;
+        m_Entity.m_speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (m_Entity.m_health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        if (m_target == null)
+        {
+            return;
+        }
+        else
+        {
+            //Vector2 m_wayPT = new Vector2(m_target.transform.position.x - transform.position.x, m_target.transform.position.y - transform.position.y);
+            ////float ComLength = Mathf.Sqrt((m_wayPT.x * m_wayPT.x) + (m_wayPT.y * m_wayPT.y));
+            //m_wayPT.Normalize();
+            //// Compute Dot Product
+            //float DotProduct = ((m_wayPT.x * transform.rotation.x) + (m_wayPT.y * transform.rotation.y));
+            //if (DotProduct < 0.999f)
+            //{
+            //    transform.Rotate(new Vector3(m_target.transform.position.x, m_target.transform.position.y, 0) * Time.deltaTime * m_Entity.m_speed);
+            //}
+            Quaternion m_rotation = Quaternion.LookRotation(new Vector3(m_target.transform.position.x, m_target.transform.position.y, m_target.transform.position.z)+ new Vector3(transform.position.x, transform.position.y, transform.position.z));
+            transform.rotation = Quaternion.Slerp(new Quaternion(transform.rotation.x, transform.rotation.y, 0, 0), new Quaternion(m_rotation.x, m_rotation.y,0,0), Time.deltaTime * m_Entity.m_speed);
+        }
+        if (m_Entity.m_attackCooldown < 0.0f)
+        {
+                Attack();
+                m_Entity.m_attackCooldown = 2.0f;
+        }
+        m_Entity.m_attackCooldown -= Time.deltaTime;
+	}
+
+    void OnTriggerStay2D(Collider2D target)
+    {
+        if (!m_isNecro && target.tag == "Player")
+        {
+            m_target = target.gameObject;
+        }
+        else if (m_isNecro && target.tag == "Enemy")
+        {
+             m_target = target.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D target)
+    {
+        m_Entity.m_attackCooldown = 2.0f;
+        m_target = null;
+    }
+
+    void Attack()
+    {
+        if (m_proj)
+        {
+            Projectile clone = Instantiate(m_proj, new Vector3(transform.position.x-.05f, transform.position.y-0.5f, 0), Quaternion.identity) as Projectile;
+            Physics2D.IgnoreCollision(clone.collider2D, this.collider2D);
+            clone.rigidbody2D.velocity = -m_target.transform.position * m_Entity.m_speed;
+        }
+    }
+}
---
+++ Straw_Man/Assets/Visible.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Visible : MonoBehaviour {
+
+    //public GameObject[] m_this = new GameObject[10];
+    public GameObject wolf;
+    public List<GameObject> m_objects;
+    public List<Transform> m_objectPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //m_this[0] = wolf;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //for (int i = 0; m_this[i]; i++)
+        //{
+        //    if (m_this[i] != null)
+        //    {
+        //        m_objects.Add(m_this[i]);
+        //        m_objectPos.Add(m_this[i].transform);
+        //    }
+
+        m_objects.Add(wolf);
+        m_objectPos.Add(wolf.transform);
+        for (int i = 0; i < m_objects.Count; i++)
+        {
+            if (m_objects[i].renderer.isVisible)
+            {
+                Instantiate(m_objects[i], m_objectPos[i].position, m_objectPos[i].rotation);
+            }
+            else if (!m_objects[i].renderer.isVisible)
+            {
+                Destroy(m_objects[i]);
+            }
+        }
+	}
+}
