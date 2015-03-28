5fc2f23d60fd1db5f1c2d5e7712228b1e7ea5095
Duriel Harris <djharris_07@yahoo.com>
2015-03-27 22:55:41 -0400

    Code Freeze

---
+++ Straw_Man/Assets/LoseScenChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseScenChange : MonoBehaviour {
+
+
+    public string scene;
+    public int sceneNum;
+    private bool LoadLock;
+    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && !LoadLock)
+            LoadScene();
+    }
+    void LoadScene()
+    {
+        Application.LoadLevel(9);
+    }
+}
---
+++ Straw_Man/Assets/WinSceneChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinSceneChange : MonoBehaviour 
+{
+
+    public string scene;
+    public int sceneNum;
+    private bool LoadLock;
+	// Use this for initialization
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && !LoadLock)
+            LoadScene();
+    }
+    void LoadScene()
+    {
+        Application.LoadLevel(9);
+    }
+}
