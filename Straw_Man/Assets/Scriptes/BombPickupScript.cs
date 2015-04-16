a9c00c7262d006f33b11f7c0b556e8b0bfd216d4
Duriel Harris <djharris_07@yahoo.com>
2015-04-16 19:18:56 -0400

    ljxhf

---
+++ Straw_Man/Assets/Scriptes/BombPickupScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombPickupScript : MonoBehaviour 
+{
+    void OnTriggerEnter2D(Collider2D _collider)
+    {
+        if (_collider.gameObject.tag == "Player")
+        {
+            _collider.gameObject.GetComponent<PlayerInventory>().AddBomb();
+            Destroy(gameObject);
+        }
+    }
+
+}
