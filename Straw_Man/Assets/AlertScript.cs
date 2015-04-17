09265fe1b7c40c4435aa4d42e606fc13baf89abb
Duriel Harris <djharris_07@yahoo.com>
2015-04-17 19:42:08 -0400

    CodeFreeze 2
    Conflicts:
    	Straw_Man/Assembly-CSharp-vs.csproj
    	Straw_Man/Assembly-CSharp.csproj
    	Straw_Man/Assets/AlertScript.cs
    	Straw_Man/Assets/Prefabs/DemonHitBOx.prefab
    	Straw_Man/Assets/Prefabs/Player/Player.prefab
    	Straw_Man/Assets/Prefabs/necromon/Demon.prefab
    	Straw_Man/Assets/Prefabs/necromon/EthrealBeing.prefab
    	Straw_Man/Assets/Prefabs/necromon/Soldier.prefab
    	Straw_Man/Assets/Scenes/DungeonScene.unity
    	Straw_Man/Straw_Man-csharp.sln
    	Straw_Man/Straw_Man.sln

--- Straw_Man/Assets/AlertScript.cs
+++ Straw_Man/Assets/AlertScript.cs
@@ -5,10 +5,21 @@ public class AlertScript : MonoBehaviour
 {
     public Demon patrol;
 
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
     void OnTriggerEnter2D(Collider2D _collider)
     {
-        if (_collider.gameObject.tag != patrol.gameObject.tag )
+        if (_collider.gameObject.tag != this.tag )
             patrol.target = _collider.gameObject;
-
     }
 }
