5598298cb95f04ddbe6fdb7539f6a9b537dcee67
Eric Truitt <erictruitt@fullsail.edu>
2015-04-17 18:44:11 -0400

    commit before code freeze 1. Finished player menu, player can now scroll through his necromon in game.

---
+++ Straw_Man/Assets/PlayerMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMenu : MonoBehaviour
+{
+    public int m_necroID;
+    public GameObject m_player;
+    public GameObject m_selectLocation;
+
+    private Color m_visible;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_selectLocation = GameObject.FindGameObjectWithTag("Respawn");
+        m_visible = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_player.GetComponent<PlayerInventory>().runes[m_necroID].collected == false)
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0.0f, 0.0f, 0.0f);
+        else if (m_player.GetComponent<PlayerInventory>().runes[m_necroID].collected == true)
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(m_visible.r, m_visible.g, m_visible.b);
+    }
+
+    void OnMouseDown()
+    {
+        if (m_player.GetComponent<PlayerInventory>().runes[m_necroID].collected)
+        {
+            m_player.GetComponent<PlayerInventory>().m_selectedRune = m_necroID;
+            m_selectLocation.SendMessage("Summon", m_necroID, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Straw_Man/Assets/PlayerMenuDisplayNecromon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMenuDisplayNecromon : MonoBehaviour 
+{
+    public GameObject[] m_Necromon;
+    public GameObject m_player, m_currentSelection = null;
+
+    public void Summon(int _ID)
+    {
+        if (m_currentSelection != null)
+        {
+            Destroy(m_currentSelection);
+            m_currentSelection = null;
+        }
+        m_currentSelection = (GameObject)Instantiate(m_Necromon[_ID], transform.position, Quaternion.identity);
+    }
+}
