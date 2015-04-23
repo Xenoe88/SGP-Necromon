a509980b9e3b86f737fa41aea07ddd15bfc4b9fe
Zeeshan Majeed <majeedzee@hotmail.com>
2015-04-23 19:02:52 -0400

    Readded all deleted files

---
+++ Straw_Man/Assets/CheckPlayerProgress.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPlayerProgress : MonoBehaviour 
+{
+    public int m_sealID;
+    public GameObject m_player;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        if (m_player.gameObject.GetComponent<PlayerScript>().m_gameStatus == m_sealID)
+        {
+            DestroyImmediate(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //temp
+        if (m_player.gameObject.GetComponent<PlayerScript>().m_gameStatus == m_sealID)
+        {
+            DestroyImmediate(gameObject);
+        }
+	}
+}
