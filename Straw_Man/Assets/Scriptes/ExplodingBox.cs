ad3fe9ac6dce411376d0b30b5a55dd336aca6204
Zeeshan Majeed <majeedzee@hotmail.com>
2015-04-26 16:00:36 -0400

    fixed some bugs from buglist

--- Straw_Man/Assets/Scriptes/ExplodingBox.cs
+++ Straw_Man/Assets/Scriptes/ExplodingBox.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class ExplodingBox : MonoBehaviour
 {
+    void Update()
+    {
+        Destroy(this.gameObject, 0.5f);
+    }
     void OnTriggerEnter2D(Collider2D _target)
     {
         _target.SendMessage("ModifyHealth", -50,SendMessageOptions.DontRequireReceiver);
